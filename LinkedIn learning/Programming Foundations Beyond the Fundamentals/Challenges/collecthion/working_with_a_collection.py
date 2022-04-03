3d39169962fa05fa784cc4d5dcb34856d530dbe6
3dnan-AT <3dnan.mohammad.2oo2@gmail.com>
2022-04-03 07:07:28 +0300

    Done the fists challenge in the socend course

---
+++ LinkedIn learning/Programming Foundations Beyond the Fundamentals/Challenges/collecthion/working_with_a_collection.py
@@ -0,0 +1,34 @@
+# Nearest stars to Earth
+
+star1 = 'Sol'
+star2 = 'Alpha Centauri'
+star3 = 'Barnard'
+star4 = 'Wolf 359'
+
+stars = ['Sol',
+         'Alpha Centauri',
+         'Barnard',
+         'Wolf 359']
+
+print(stars[3])
+
+# highest peak on each tectinic plate
+African = 'Kilimangaro'
+Antarctic = 'Vinson'
+Australian = 'Puncak Jaya'
+Eursian = 'Everest'
+North_American = 'Denali'
+Pacific = 'Mauna Kea'
+South_American = 'Aconcagua'
+
+peaks = {
+    African: 'Kilimangaro',
+    Antarctic: 'Vinson',
+    Australian: 'Puncak Jaya',
+    Eursian: 'Everest',
+    North_American: 'Denali',
+    Pacific: 'Mauna Kea',
+    South_American: 'Aconcagua'
+}
+
+print(peaks[Pacific])
