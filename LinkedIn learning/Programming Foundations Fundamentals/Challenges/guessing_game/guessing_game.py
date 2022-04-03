e494b8620297af557e57e0311856144847206b9f
3dnan-AT <3dnan.mohammad.2oo2@gmail.com>
2022-04-03 06:47:10 +0300

    answer the gussing game challenge

---
+++ LinkedIn learning/Programming Foundations Fundamentals/Challenges/guessing_game/guessing_game.py
@@ -0,0 +1,10 @@
+my_food = "Apple"
+
+answer = input("What's my favorite food?: ")
+
+if answer == my_food:
+    print("Yep! So amazing!")
+else:
+    print("Yuck! Than's not it!")
+
+print("Thanks for playing!")
